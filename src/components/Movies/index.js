daaae1495b477acad881c5814661b3d38794d2a2
jhojun7 <jhojun@gmail.com>
2023-04-17 00:53:44 +0900

    feat: image, text hover, item.year까지

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,36 +1,37 @@
-import { useState, useEffect } from "react"
-import "./style.css"
+import { useState, useEffect } from "react";
+import "./style.css";
 
 export default function Movies() {
   // JS
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     // fetch("https://yts.mx/api/v2/list_movies.json")
     fetch("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
       .then((res) => res.json())
-      .then((json) => setMovies(json.data.movies))
-  }, [])
+      .then((json) => setMovies(json.data.movies));
+  }, []);
 
   const render = movies.map((item) => {
     return (
       <div className="movie" key={item.id}>
         <div className="movieDetail">
           <h2 className="movieTitle">{item.title}</h2>
+          <p className="movieYear">{item.year}</p>
         </div>
         <img
           className="movieImage"
-          src={item.medium_cover_image}
+          src={item.large_cover_image}
           alt={item.title}
         />
       </div>
-    )
-  })
+    );
+  });
 
   // XML
   return (
     <>
       <div>{render}</div>
     </>
-  )
+  );
 }
