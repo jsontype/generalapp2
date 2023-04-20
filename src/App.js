f8ff4070be4612160afe2dd1fa38cf4a29775604
MINSOO LEE <minsoolee@MINSOOui-MacBookAir.local>
2023-04-21 06:42:04 +0900

    todo추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { useState } from "react"
 import TopMenu from "./components/TopMenu"
 import Movies from "./components/Movies"
 import Counter from "./components/Counter"
+import Todos from "./components/Todos"
 
 export default function App() {
   // JS
   const [openMovies, setOpenMovies] = useState(false)
   const [openCounter, setOpenCounter] = useState(false)
+  const [openTodos, setOpenTodos] = useState(false)
 
   // XML
   return (
@@ -17,6 +19,8 @@ export default function App() {
         setOpenMovies={setOpenMovies}
         openCounter={openCounter}
         setOpenCounter={setOpenCounter}
+        openTodos={openTodos}
+        setOpenTodos={setOpenTodos}
       />
       {openMovies && <Movies />}
       {openCounter && (
@@ -24,6 +28,7 @@ export default function App() {
           <Counter />
         </>
       )}
+      {openTodos && <Todos />}
     </>
   )
 }
